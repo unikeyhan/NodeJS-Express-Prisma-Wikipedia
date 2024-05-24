c8d6814b0d4f03dd6c28d141dd0c091506507713
Alireza Shahryari <eng.alirezashahryari@gmail.com>
2024-05-24 16:25:15 +0330

    modify InternalServerError message in AllException Handler

--- src/common/exception/all-exception.handler.ts
+++ src/common/exception/all-exception.handler.ts
@@ -5,7 +5,7 @@ function AllExceptionHandler(app: Express)  {
     let status = err?.status ?? err?.statusCode ?? err?.code;
     if (!status || isNaN(+status) || status > 511 || status < 200) status = 500;
     res.status(status).json({
-      message: err?.message ?? err?.stack ?? t('InternalServerError'),
+      message: err?.message ?? err?.stack ?? t('http.InternalServerError'),
     });
   });
 }
